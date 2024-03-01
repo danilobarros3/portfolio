a126f95f3817987eee3cf6cfee0b8acee6e0f11b
danilobarros3 <danilo.lionsoft@gmail.com>
2024-03-01 15:04:27 -0300

    npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import Navbar from "./components/Navbar"
 import Info from "./components/Info";
-import About from"./components/About";
+import About from "./components/About";
 import Tecnologies from "./components/Tecnologies";
 import Skills from "./components/Skills";
 import Register from "./components/Register";
 import Footer from "./components/Footer";
 function App() {
-  return(
+  return (
     <div className="App">
-        <Navbar/>
+      <Navbar />
       <div className="container">
-        <Info/>
-        <About/>
-        <Tecnologies/>
-        <Skills/>
-        <Register/>
-        <Footer/>
-        </div>
+        <Info />
+        <About />
+        <Tecnologies />
+        <Skills />
+        <Register />
+        <Footer />
+      </div>
     </div>
   )
 }
