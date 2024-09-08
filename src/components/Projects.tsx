200af0c795d2253f2358387a9cefad5b5c69d63c
danilobarros3 <danilo.lionsoft@gmail.com>
2024-09-08 16:04:17 -0300

    :bug: fix: resolvin carousel previous and next mobile

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,7 +27,6 @@ interface GitHubRepo {
 export function Projects() {
   const [projects, setProjects] = useState<Repo[]>([]);
   const [_, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
 
   const getDataProject = async () => {
     setLoading(true);
@@ -41,10 +40,9 @@ export function Projects() {
         link: repo.html_url,
       }));
       setProjects(formattedProjects);
-      toast.success("Projetos carregados com sucesso");
     } catch (error) {
       console.error(error);
-      setError("Erro ao buscar projetos do GitHub");
+      toast.error("Erro ao buscar projetos do GitHub");
     } finally {
       setLoading(false);
     }
@@ -58,52 +56,50 @@ export function Projects() {
 
   return (
     <div
-  id="projects"
-  className="bg-primary py-4 px-4 md:px-16 lg:px-24 flex flex-col items-center gap-6 md:gap-10"
->
-  <h1 className="text-[#70FF00] text-3xl md:text-4xl font-bold text-center">
-    {t("projects")}( )
-  </h1>
-  {error && <p className="text-red-500 text-center">{error}</p>}
-  <div className="w-full max-w-5xl">
-    <Carousel>
-      <CarouselContent className="gap-4 md:gap-6 lg:gap-8 p-4 md:p-0">
-        {projects.map((repo) => (
-          <CarouselItem
-            key={repo.name}
-            className="p-2 md:p-4 max-w-full sm:max-w-[90%] md:max-w-[45%] lg:max-w-[30%] mx-auto"
-          >
-            <div className="grid gap-4 md:gap-6 items-center text-white">
-              <img
-                src={projectsImage}
-                alt={repo.name}
-                className="w-full h-auto rounded-lg"
-              />
-              <div className="text-center">
-                <h2 className="font-bold text-lg md:text-xl">
-                  {repo.name}
-                </h2>
-                <p className="mt-2 mb-4 text-sm md:text-base">
-                  {repo.description}
-                </p>
-                <a
-                  href={repo.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-[#70FF00] hover:underline"
-                >
-                  See on GitHub
-                </a>
-              </div>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="text-white hover:text-[#70ff00]" />
-      <CarouselNext className="text-white hover:text-[#70ff00]" />
-    </Carousel>
-  </div>
-</div>
-
+      id="projects"
+      className="bg-primary py-4 px-4 md:px-16 lg:px-24 flex flex-col items-center gap-6 md:gap-10"
+    >
+      <h1 className="text-[#70FF00] text-3xl md:text-4xl font-bold text-center">
+        {t("projects")}( )
+      </h1>
+      <div className="w-full max-w-5xl">
+        <Carousel>
+          <CarouselContent className="gap-4 md:gap-6 lg:gap-8 p-4 md:p-0">
+            {projects.map((repo) => (
+              <CarouselItem
+                key={repo.name}
+                className="p-2 md:p-4 max-w-full sm:max-w-[90%] md:max-w-[45%] lg:max-w-[30%] mx-auto"
+              >
+                <div className="grid gap-4 md:gap-6 items-center text-white">
+                  <img
+                    src={projectsImage}
+                    alt={repo.name}
+                    className="w-full h-auto rounded-lg"
+                  />
+                  <div className="text-center">
+                    <h2 className="font-bold text-lg md:text-xl">
+                      {repo.name}
+                    </h2>
+                    <p className="mt-2 mb-4 text-sm md:text-base">
+                      {repo.description}
+                    </p>
+                    <a
+                      href={repo.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-[#70FF00] hover:underline"
+                    >
+                      See on GitHub
+                    </a>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden md:block text-white hover:text-[#70ff00]" />
+          <CarouselNext className="hidden md:block text-white hover:text-[#70ff00]" />
+        </Carousel>
+      </div>
+    </div>
   );
 }
